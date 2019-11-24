b48c893b9a7a3f36d398ee5c012a703093f3cdba
Dawid Majdanik <dawidmajdanik96@gmail.com>
2019-11-24 12:58:56 +0100

    WIP: Removed unused inputs MovementController cleanup

--- VR_Escape_Museum/Assets/_MAIN/Scripts/Keys.cs
+++ VR_Escape_Museum/Assets/_MAIN/Scripts/Keys.cs
@@ -5,22 +5,7 @@ using UnityEngine;
 public class Keys : MonoBehaviour
 {    public static class Inputs
     {
-        public static string MOUSE_SCROLL = "Mouse ScrollWheel";
-        public static string MOUSE_X_AXIS = "Mouse X";
-        public static string MOUSE_Y_AXIS = "Mouse Y";
-        public static string KEYBOARD_HORIZONTAL = "Horizontal";
-        public static string KEYBOARD_VERTICAL = "Vertical";
-        public static string ROTATION_AXIS = "Rotation";
-
-
         public static int LEFT_MOUSE_CLICKED = 0;
         public static int RIGHT_MOUSE_CLICKED = 1;
-        public static int SCROLL_CLICKED = 2;
-        public static float TERRAIN_TEMP_MIN_HEIGHT = -10;
-        public static float TIME_OF_LONG_CLICK = 1;
-        public static float TIME_OF_DOUBLE_CLICK = 1;
-        public static float CLICK_DELAY = 0.5f;
-
-        public const float MAX_RAY_DISTANCE = 500;
     }
 }
--- VR_Escape_Museum/Assets/_MAIN/Scripts/MovementController.cs
+++ VR_Escape_Museum/Assets/_MAIN/Scripts/MovementController.cs
@@ -21,32 +21,7 @@ public class MovementController : MonoBehaviour
     [SerializeField] private GameObject handPrefab;
     [SerializeField] private GameObject coloredHandPrefab;
     [SerializeField] private float RayLenght = 3f;
-    [SerializeField] private GameObject pauseObject;
-    [SerializeField] private GameObject endGameObject;
 
-    private GameObject cursorInstance;
-    private GameObject teleportInstance;
-    private GameObject handInstance;
-    private GameObject coloredHandInstance;
-    private bool pause = false;
-    private static float a = -1.2f;
-    private static float b = 1.5f;
-    private static float c = 5.0f;
-    Vector3 endPos = new Vector3(a, b, c);
-
-    void Start()
-    {
-        Time.timeScale = 1;
-        cursorInstance = Instantiate(gameCursorPrefab);
-        teleportInstance = Instantiate(teleportPrefab);
-        handInstance = Instantiate(handPrefab);
-        coloredHandInstance = Instantiate(coloredHandPrefab);
-        cursorInstance.SetActive(true);
-        teleportInstance.SetActive(false);
-        handInstance.SetActive(false);
-        coloredHandInstance.SetActive(false);
-        pauseObject.SetActive(false);
-    }
 
     public void MovementUpdate(InputController.InputValues inputValues)
     {
@@ -60,28 +35,28 @@ public class MovementController : MonoBehaviour
         Ray ray = new Ray(viewCamera.transform.position, viewCamera.transform.rotation * Vector3.forward);
         RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag == "Ground" && Physics.Raycast(ray, out hit, RayLenght))
             {
-                if (hit.collider.tag == "Ground" && Physics.Raycast(ray, out hit, RayLenght))
-                {
-                    // If the ray hits something, set the position to the hit point and rotate based on the normal vector of the hit
-                    teleportInstance.SetActive(true);
-                    cursorInstance.SetActive(false);
-                    teleportInstance.transform.position = hit.point;
-                    teleportInstance.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
-                }
-                else
-                {
-                    teleportInstance.SetActive(false);
-                    cursorInstance.SetActive(true);
-                }
+                // If the ray hits something, set the position to the hit point and rotate based on the normal vector of the hit
+                teleportPrefab.SetActive(true);
+                gameCursorPrefab.SetActive(false);
+                teleportPrefab.transform.position = hit.point;
+                teleportPrefab.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
             }
             else
             {
-                // If the ray doesn't hit anything, set the position to the maxCursorDistance and rotate to point away from the camera
-                cursorInstance.transform.position = ray.origin + ray.direction.normalized;
-                cursorInstance.transform.rotation = Quaternion.FromToRotation(Vector3.up, -ray.direction);
-            }        
+                teleportPrefab.SetActive(false);
+                gameCursorPrefab.SetActive(true);
+            }
+        }
+        else
+        {
+            // If the ray doesn't hit anything, set the position to the maxCursorDistance and rotate to point away from the camera
+            gameCursorPrefab.transform.position = ray.origin + ray.direction.normalized;
+            gameCursorPrefab.transform.rotation = Quaternion.FromToRotation(Vector3.up, -ray.direction);
+        }
     }
 
     private void CheckInput()
@@ -94,9 +69,9 @@ public class MovementController : MonoBehaviour
 
     public void Teleport()
     {
-        if (teleportInstance.activeInHierarchy)
+        if (teleportPrefab.activeInHierarchy)
         {
-            Vector3 markerPosition = teleportInstance.transform.position;
+            Vector3 markerPosition = teleportPrefab.transform.position;
             Player.position = new Vector3(markerPosition.x, Player.position.y, markerPosition.z);
         }
     }
