045419bdf8997b78cdf87e45a601199971a1a2f2
Merafil <merafilkaven@gmail.com>
2019-12-03 00:17:33 +0100

    Ahtung : All Objects are interactable rn WIP: ExamineSystem To Do : Implement correct rotations Eventually refractor by making camera controller

--- VR_Escape_Museum/Assets/_MAIN/Scripts/ExamineSystem/ExamineSystem.cs
+++ VR_Escape_Museum/Assets/_MAIN/Scripts/ExamineSystem/ExamineSystem.cs
@@ -1,46 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExamineSystem : MonoBehaviour
 {
-
+    [SerializeField] GameObject player;
     [SerializeField] Camera mainCam; 
     [SerializeField] GameObject examineCanvas;
     GameObject clickedObject;
 
+    [SerializeField] Button upButton;
+    [SerializeField] Button downButton;
+    [SerializeField] Button leftButton;
+    [SerializeField] Button rightButton;
+    [SerializeField] Button exitButton;
+
     //Holds Original Postion And Rotation So The Object Can Be Replaced Correctly
-    Vector3 originalPosition;
-    Vector3 originalRotation;
+    Vector3 objectOriginalPosition;
+    Vector3 objectOriginalRotation;
+
+    Vector3 playerOriginalPosition;
+    Vector3 playerOriginalRotation;
 
     //If True Allow Rotation Of Object
     bool examineMode;
 
-    void Awake() {
-        
-    }
     void Start() {
+        
         mainCam = Camera.main;
         examineMode = false;
-        
+
+        upButton.onClick.AddListener(RotateUp);
+        downButton.onClick.AddListener(RotateDown);
+        leftButton.onClick.AddListener(RotateLeft);
+        rightButton.onClick.AddListener(RotateRight);
+        exitButton.onClick.AddListener(ExitExamineMode);
+
     }
 
     private void Update() {
 
-        ClickObject();//Decide What Object To Examine
-
-        TurnObject();//Allows Object To Be Rotated
-
-        ExitExamineMode();//Returns Object To Original Postion
+        ClickObject();
     }
+
 
 
     void ClickObject() {
         if(Input.GetMouseButtonDown(0) && examineMode == false) {
+            
             RaycastHit hit;
             Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
 
@@ -48,41 +59,60 @@ public class ExamineSystem : MonoBehaviour
                 //ClickedObject Will Be The Object Hit By The Raycast
                 clickedObject = hit.transform.gameObject;
 
-                //Save The Original Postion And Rotation
-                originalPosition = clickedObject.transform.position;
-                originalRotation = clickedObject.transform.rotation.eulerAngles;
+                
+                objectOriginalPosition = clickedObject.transform.position;
+                objectOriginalRotation = clickedObject.transform.rotation.eulerAngles;
+                playerOriginalPosition = player.transform.position;
+                playerOriginalRotation = player.transform.rotation.eulerAngles;
 
-                //Now Move Object In Front Of Camera
-                clickedObject.transform.position = mainCam.transform.position + (transform.forward * 2f);
+                //Move player to location used to inspect items
+                player.transform.position = new Vector3(-20, 1, -1);
+                mainCam.transform.localEulerAngles = new Vector3(0, 0, 0);
+                
+
+                clickedObject.transform.position = player.transform.position - (transform.forward * 1.4f);
 
 
-              //  examineCanvas.SetActive(true);
-                //Turn Examine Mode To True
+                examineCanvas.SetActive(true);
+
                 examineMode = true;
             }
         }
     }
 
-    void TurnObject() {
-        if(Input.GetMouseButton(0) && examineMode) {
-            float rotationSpeed = 15;
+    //temp
+    private void RotateUp() {
 
-            float xAxis = Input.GetAxis("Mouse X") * rotationSpeed;
-            float yAxis = Input.GetAxis("Mouse Y") * rotationSpeed;
+        clickedObject.transform.Rotate(Vector3.left * 10);
 
-            clickedObject.transform.Rotate(Vector3.up, -xAxis, Space.World);
-            clickedObject.transform.Rotate(Vector3.right, yAxis, Space.World);
-        }
     }
 
+    private void RotateDown() {
+
+        clickedObject.transform.Rotate(Vector3.right * 10);
+
+    }
+    private void RotateLeft() {
+
+        clickedObject.transform.Rotate(Vector3.up * 10);
+
+    }
+    private void RotateRight() {
+
+        clickedObject.transform.Rotate(Vector3.down * 10);
+
+    }
+
+
     void ExitExamineMode() {
-        if(Input.GetMouseButtonDown(1) && examineMode) {
+        if( examineMode) {
             
-            clickedObject.transform.position = originalPosition;
-            clickedObject.transform.eulerAngles = originalRotation;
+            clickedObject.transform.position = objectOriginalPosition;
+            clickedObject.transform.eulerAngles = objectOriginalRotation;
+            player.transform.position = playerOriginalPosition;
+            player.transform.eulerAngles = playerOriginalRotation;
 
-
-            
+            examineCanvas.SetActive(false);
             examineMode = false;
         }
     }
