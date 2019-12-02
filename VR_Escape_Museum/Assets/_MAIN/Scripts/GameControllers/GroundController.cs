91f9deba45fbe03409736c82617b8bc741e845c6
Dawid Majdanik <dawidmajdanik96@gmail.com>
2019-12-03 00:10:11 +0100

    Moved teleport to GroundController

--- VR_Escape_Museum/Assets/_MAIN/Scripts/GameControllers/GroundController.cs
+++ VR_Escape_Museum/Assets/_MAIN/Scripts/GameControllers/GroundController.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class GroundController : MonoBehaviour
 {
+    private GameController gameController;
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform camera;
+    [SerializeField] private Transform inspectingRoomPosition;
 
+    private Vector3 currentPosition;
+    private Vector3 currentRotation;
+
+    public void Init(GameController _gameController)
+    {
+        gameController = _gameController;
+    }
+
+    public void TeleportPlayer(Vector3 _position)
+    {
+        player.position = new Vector3(_position.x, player.position.y, _position.z);
+    }
+
+    public void TeleportPlayerToInspectingRoom()
+    {
+        currentPosition = player.position;
+        currentRotation = camera.localEulerAngles;
+        player = inspectingRoomPosition;
+    }
+
+    public void TeleportPlayerToGame()
+    {
+
+    }
 }
